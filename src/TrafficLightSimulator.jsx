9df9670b2cab9b7aac6520755b8667ee87762d86
Mohsin563 <mohsan5panhwar@gmail.com>
2025-06-02 23:54:16 -0700

    Use effect

--- src/TrafficLightSimulator.jsx
+++ src/TrafficLightSimulator.jsx
@@ -3,18 +3,26 @@ import React, { useState, useEffect } from "react";
 const TrafficLightSimulator = () => {
   const [activeLight, setActiveLight] = useState("red");
 
+  useEffect(() => {
+    const changeLights = () => {
+      setActiveLight((prev) =>
+        prev === "red" ? "yellow" : prev === "yellow" ? "green" : "red"
+      );
+    };
+    
+  }, 3000);
 
   return (
-   <>
-   <div>Traffic Light simulator</div>
+    <>
+     <div>Traffic Light simulator</div>
     <div style={styles.container}>
       <div style={styles.trafficLight}>
-        <div style={{  backgroundColor: activeLight === "red" ? "red" : "gray" }}>red</div>
-        <div style={{  backgroundColor: activeLight === "yellow" ? "yellow" : "gray" }}>yellow</div>
-        <div style={{  backgroundColor: activeLight === "green" ? "green" : "gray" }}>gray</div>
+        <div style={{ ...styles.light, backgroundColor: activeLight === "red" ? "red" : "gray" }}>red</div>
+        <div style={{ ...styles.light, backgroundColor: activeLight === "yellow" ? "yellow" : "gray" }}>yellow</div>
+        <div style={{ ...styles.light, backgroundColor: activeLight === "green" ? "green" : "gray" }}>gray</div>
       </div>
     </div>
-  </>
+    </>
   );
 };
 
@@ -35,7 +43,12 @@ const styles = {
     borderRadius: "10px",
     alignItems: "center",
   },
-  
+  light: {
+    width: "50px",
+    height: "50px",
+    margin: "5px",
+    borderRadius: "50%",
+  },
 };
 
 export default TrafficLightSimulator;
